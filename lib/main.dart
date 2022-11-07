ea5e54bd524e6e6f0d5f3a9479409602045c7855
Sardor Ergashaliyev <sardorergashaliyev@gmail.com>
2022-11-07 18:41:29 +0500

    refreshing

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,40 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Container(
-      color: Colors.blue,
-      child: const Center(
-          child: Text(
-        "Sardor",
-        style: TextStyle(color: Colors.yellow),
-      )),
-    ));
+            color: Colors.black,
+            child: Center(
+              child: Column(
+                children: [
+                  Container(
+                    child: Text('U', textAlign: TextAlign.center),
+                    height: 100,
+                    width: 500,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    height: 10,
+                    width: 500,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    child: Text('Z', textAlign: TextAlign.center,),
+                    width: 500,
+                    height: 100,
+                    color: Colors.white,
+                  ),
+                  Container(
+                    height: 10,
+                    width: 500,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    child: Text('B', style: TextStyle(decoration: TextDecoration.none), textAlign: TextAlign.center,),
+                    height: 100,
+                    width: 500,
+                    color: Colors.green,
+                  )
+                ],
+              ),
+            )));
   }
 }
